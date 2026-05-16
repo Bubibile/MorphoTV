2a82453ad82dbee29c06ef98b39898cb9cd7effa
Bubibile <122532123+Bubibile@users.noreply.github.com>
2026-05-16 12:22:25 +0800

    Update douban-media.tsx

--- src/components/douban-media.tsx
+++ src/components/douban-media.tsx
@@ -178,7 +178,7 @@ export function DoubanMedia() {
             >
               <div className="relative w-full h-40 md:h-48">
                 <img
-                  src={`https://weserv.nl{encodeURIComponent(movie.cover)}`}
+                  src={`https://weserv.nl/{encodeURIComponent(movie.cover)}`}
                   alt={movie.title}
                   className="absolute inset-0 w-full h-full object-cover"
                   referrerPolicy="no-referrer"
